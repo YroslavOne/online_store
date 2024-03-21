99271fa630a17e8e0b83fc9ba19e73c0b5fa32a9
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2024-03-21 22:42:14 +0700

    22.42 21.03.2024

---
+++ pizza-app/src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonProps } from '../Button.props';
+import './Button.module.css';
+import cn from 'classnames';
+// import { FC } from 'react';
+
+// export const ButtonAlt: FC<ButtonProps> = ({ children, className, ...props }) => {
+//   return (
+//     <button className={cn('button accent', className)} {...props}>
+//       {children}
+//     </button>
+//   );
+// };
+
+function Button({ children, className, ...props }: ButtonProps) {
+  return (
+    <button className={cn('button accent', className)} {...props}>
+      {children}
+    </button>
+  );
+}
+export default Button;
